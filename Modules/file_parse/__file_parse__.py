f854ecfe35450beb2db3431ce8d6dc2d182cd121
David Kroell <dkroell@acm.org>
2015-06-09 05:17:59 -0400

    adding a cred parsing module

---
+++ Modules/file_parse/__file_parse__.py
@@ -0,0 +1,39 @@
+
+
+
+#methods for obtaining credentials from a sample file.
+#The paramemter passed is the file itself once opened.
+#these methods are based on the sample of how the credentials will be sent remotely.
+
+def obt_login(text):
+	first_line = text[0]
+	line_list = first_line.split(',')
+#indentation can cause an error
+	return line_list[0].replace("	", "")
+
+def obt_pass(text):
+        first_line = text[0]
+        line_list = first_line.split(',')
+        return line_list[1].replace(" ", "")
+	
+def obt_message(text):
+        mess_line = text[2]
+	return mess_line
+
+
+#file concatination:
+file = open("sample.txt")
+text = file.readlines()
+
+#Printing the test file credentials
+
+usr_login = obt_login(text)
+print(usr_login)
+
+usr_pass = obt_pass(text)
+print(usr_pass)
+
+usr_mess = obt_message(text)
+print(usr_mess)
+
+
