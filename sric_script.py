30934aa559a1e0ac43d9e6b3a4a2de50e889b4ef
tarrenj <tarrenj@users.noreply.github.com>
2015-06-14 17:35:01 -0400

    Update sric_script.py
    Changes things!

--- sric_script.py
+++ sric_script.py
@@ -18,15 +18,14 @@ team_dir = 'auvsi/team1/'
 # Loop forever!
 while True:
 
-#	# Verify connection
-#	# Does ftplib automatically check connections?  If so we should remove this whole block and set ftplib's timeout to like, 1s
-#	output == subprocess.check_output(['ping', '-n', '1', server]) # Need to fix this command to work on linux....
-#	if output = 'tmp string for network down':
-#		continue
+	# Verify connection before attempting to create FTP instance so we don't have to wait for 30 second timeout
+	# Does ftplib automatically check connections?  If so we should remove this whole block and set ftplib's timeout to like, 1s
+	output == subprocess.check_output(['ping', '-n', '1', server]) # Need to fix this command to work on linux....
+	if output = 'tmp string for network down':
+		continue
 
-	# We don't need to verify that we have a valid connection, because ftplib will do that for us.
 	try:
-		ftp = ftplib.FTP(ftp_addr, timeout = 5)
+		ftp = ftplib.FTP(ftp_addr, timeout = 30)
 		# Set up the FTP object
 		ftp.set_debuglevel(2)
 
@@ -44,17 +43,14 @@ while True:
 	
 	# Need to do more testing with this line, we might be able to just use storelines or retrlines...
 #	ftp.retrbinary('RETR', creds_file.write)
-	ftp.retrlines("RETR " + download, lambda s, w = creds_file.write: w(s + "\n"))
+	ftp.retrlines'"RETR ' + download, lambda s, w = creds_file.write: w(s + '\n'))
 	
 	# Parse it mytext reads all lines of the file contents
 	text = creds_file.readlines()
-		
-	up_user = fileparse.obt_login(text)
-	up_pass = fileparse.obt_pass(text)
-	up_mess = fileparse.obt_message(text)
 	
 	# Login with the new creds and put the file
-	ftp.login(up_user, up_pass)
+	ftp.quit()
+	ftp = FTP(ftp_addr, obt_login(text), obt_pass(text), timeout = 30)
 	ftp.storelines('STOR', upload_name, upload)
 	
 	# End this script to save resources for other things
