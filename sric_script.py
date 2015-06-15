18557a72964aed0e092d1b5a50b668963fca5695
Jake Tarren <jake.tarren@gmail.com>
2015-06-14 21:41:47 -0400

    Why am I still working on this?

--- sric_script.py
+++ sric_script.py
@@ -5,15 +5,15 @@ import Modules.file_parse.fileparse
 
 
 # The IP of the server we're downloading/uploading things to/from
-ftp_addr = '192.168.1.200'
+ftp_adr = '10.48.22.113'
 
 # The names of the files were dealing with
-download = 'team1.txt'
+download = 'cnu.txt'
 upload = open('upload_package.txt', 'r')
 creds_file = open('credentials.txt', 'wb')
 
 # Where we want to put the file
-upload_name = 'CNU/IMPRINT_upload_package.txt'
+upload_name = 'CNU-IMPRINT_upload_package.txt'
 #team_dir = 'auvsi/team1/'
 
 # Loop forever!
@@ -21,7 +21,7 @@ while True:
 
 	# Verify connection before attempting to create FTP instance so we don't have to wait for 30 second timeout
 	# Does ftplib automatically check connections?  If so we should remove this whole block and set ftplib's timeout to like, 1s
-	output = subprocess.check_output(['ping', '-c', '1', ftp_addr])
+	output = subprocess.check_output(['ping', '-w2', ftp_addr])
 	if output == 'tmp string for network down':
 		continue
 
@@ -43,8 +43,8 @@ while True:
 		ftp.cwd(team_dir)
 	
 	# Need to do more testing with this line, we might be able to just use storelines or retrlines...
-#	ftp.retrbinary('RETR', creds_file.write)
-	ftp.retrlines('RETR ' + download, lambda s, w = creds_file.write: w(s + '\n'))
+	ftp.retrlines('RETR ' + download, creds_file.write)
+#	ftp.retrlines('RETR ' + download, lambda s, w = creds_file.write: w(s + '\n'))
 	
 	# Parse it mytext reads all lines of the file contents
 	text = creds_file.readlines()
@@ -52,7 +52,7 @@ while True:
 	# Login with the new creds and put the file
 	ftp.quit()
 	ftp = FTP(ftp_addr, obt_login(text), obt_pass(text), timeout = 30)
-	ftp.storelines('STOR', upload_name, upload)
+	ftp.storlines('STOR ', upload_name, upload)
 	
 	# End this script to save resources for other things
 	ftp.quit()
