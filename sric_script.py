c20d71fa1e6c9a39a9e6d2fd66989af45749a583
Jake Tarren <jake.tarren@gmail.com>
2015-06-17 20:09:22 -0400

    fixes all the things!

--- sric_script.py
+++ sric_script.py
@@ -1,16 +1,31 @@
+import sys
 import subprocess
 import ftplib  # I'd rather use ftputil, since it's a nice wrapper for ftplib, but it might make things slower?
-from ftplib import FTP
-import Modules.file_parse.fileparse as fp
+#from ftplib import FTP
 
+def obt_login(text):
+    first_line = text[0]
+    line_list = first_line.split(',')
+#indentation can cause an error
+    return line_list[0].replace(" ", "")
+
+def obt_pass(text):
+        first_line = text[0]
+        line_list = first_line.split(',')
+        return line_list[1].replace(" ", "")
+    
+def obt_message(text):
+        mess_line = text[1]
+    	return mess_line
 
 # The IP of the server we're downloading/uploading things to/from
 ftp_addr = '192.168.2.2'
 
 # The names of the files were dealing with
-download = 'cnu.txt'
-upload = open('upload_package.txt', 'w')
-creds_file = open('credentials.txt', 'wb')
+download = 'auth.txt'
+#upload = open('upload_package.txt', 'r')
+filer= open('/usr/bin/upload_package.jpg', 'rb')
+creds_file = open('credentials', 'wb')
 
 # Where we want to put the file
 upload_name = 'CNU-IMPRINT_upload_package.txt'
@@ -22,8 +37,10 @@ while True:
 	# Verify connection before attempting to create FTP instance so we don't have to wait for 30 second timeout
 	
     try:
-        output = subprocess.check_output(['ping', '-w2', ftp_addr])
+        print "submodule"
+        # output = subprocess.check_output(['ping', '-w2', ftp_addr])
     except subprocess.CalledProcessError as e:
+        print "host not found"
         continue
 
 
@@ -50,22 +67,30 @@ while True:
 #	ftp.retrlines('RETR ' + download, lambda s, w = creds_file.write: w(s + '\n'))
     
     creds_file.close()
-    creds_file = open('credentials.txt', 'r+')
+    creds_file = open('credentials', 'rb')
 	
     # Parse it mytext reads all lines of the file contents
     text = creds_file.readlines()
 	
     # Login with the new creds and put the file
     ftp.quit()
-    print fp.obt_login(text)
-    print fp.obt_pass(text)
-    ftp = FTP(ftp_addr, fp.obt_login(text), fp.obt_pass(text), timeout = 30)
-    ftp.storlines('STOR', upload_name, upload.write)
-	
+
+    uname = '' + str(obt_login(text)).replace('\n', '')
+    pword = '' + str(obt_pass(text)).replace('\n', '')
+
+    ftp2 = ftplib.FTP(ftp_addr, uname, pword)
+    # ftp2 = ftplib.FTP(ftp_addr, fp.obt_login(text), fp.obt_pass(text))
+#   ftp.storlines('STOR', upload_name, upload.write)
+#   ftp.storbinary('RETR %s' % 'upload_package', upload.write)
+
+    print 'Login successful!'
+
+    ftp2.storlines('STOR ' + 'CNU-IMPRINT_upload_package.jpg', filer)
+
     # End this script to save resources for other things
-    ftp.quit()
-    cred_file.close()
-    upload.close()
+    ftp2.quit()
+    creds_file.close()
+    # upload.close()
     break
 	
 # Not currently connected... Time to try again
